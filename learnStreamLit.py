607f79372447e77e865385b5583ef459efbab690
saakethkoka <63078934+saakethkoka@users.noreply.github.com>
2022-04-11 16:08:31 -0500

    Initial commit

---
+++ learnStreamLit.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pandas as pd
+
+
+
+if 'count' not in st.session_state:
+    st.session_state.count = 0
+
+increment = st.button('Count')
+if increment:
+    st.session_state.count += 1
+
+st.write(st.session_state.count)
+
+
+df = pd.read_csv("crudeoil.csv")
+df['Date'] = pd.to_datetime(df['Date'])
+df.set_index(df.Date, inplace=True)
+
+
+
+bid_prices = st.button('Bid Price')
+ask_prices = st.button('Ask Price')
+
+
+
+
+
+if bid_prices:
+    new_df = df["Bid"]
+    st.line_chart(new_df)
+if ask_prices:
+    new_df = df["Ask"]
+    st.line_chart(new_df)
